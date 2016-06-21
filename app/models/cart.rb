fc10935e6c7afbe6b4d2c57b2ce852828cce493d
Pavel Rozhkov <prozskov@gmail.com>
2016-06-21 11:10:12 +0700

    Add full price method to cart model file

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,16 @@
 class Cart < ActiveRecord::Base
   has_many :line_items, dependent: :destroy
+
+  def full_price
+    price = 0
+    if self.line_items.any?
+      self.line_items.each do |item|
+        price = price + item.variant.price
+      end
+    else
+      price
+    end
+    price
+  end
+
 end
