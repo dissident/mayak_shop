5476298836072de850889749340d0a7d96354bce
Pavel Rozhkov <prozskov@gmail.com>
2016-06-22 13:23:57 +0700

    Remove create method from cart controller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -8,21 +8,6 @@ class CartsController < FrontendController
     end
   end
 
-  def create
-    @cart = Cart.new(cart_params)
-
-
-    respond_to do |format|
-      if @cart.save
-        format.html { redirect_to @cart, notice: 'Cart was successfully created.' }
-        format.json { render :show, status: :created, location: @cart }
-      else
-        format.html { render :new }
-        format.json { render json: @cart.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
   def update
     respond_to do |format|
       if @cart.update(cart_params)
